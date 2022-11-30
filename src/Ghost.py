44906f13007634e3fa57d9f6577acd86b4640f9c
ZynoXelek <Enzo.BROSSIER-SECHER@student.isae-supaero.fr>
2022-11-30 19:35:16 +0100

    Added variables in Ghost.py

--- src/Ghost.py
+++ src/Ghost.py
@@ -2,8 +2,15 @@ import Movable
 
 class Ghost(Movable):
     
+    powered = False
+    
     spritesNumber = 2
+    
     
     def __init__(self, x = 0, y = 0):
         super.__init__(x, y)
-        
+        self.eaten = False
+        self.current = 0
+        self.dir = 2    
+    
+    
