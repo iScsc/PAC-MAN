ad9155217cfe0fa830395f2ef6695e3366fe972d
ZynoXelek <Enzo.BROSSIER-SECHER@student.isae-supaero.fr>
2022-12-14 20:19:41 +0100

    Minor changes

--- src/Collectible.py
+++ src/Collectible.py
@@ -1,10 +1,9 @@
-import Entity
+from Entity import Entity
 
 class Collectible(Entity):
     
-    def __init__(self, x = 0, y = 0, i = 0, j = 0, maxSprite = 1, current = 0):
+    def __init__(self, x = 0, y = 0, i = 0, j = 0, maxSprite = 1, current = 0): # x,y pos / i,j sprite pos / max sprites / current sprite
         super.__init__(x, y, i, j, maxSprite, current)
         
     def collect(self):
         pass
-        
