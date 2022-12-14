d605f9894b2fd763e233ce2e0ccf885e88baa655
ZynoXelek <Enzo.BROSSIER-SECHER@student.isae-supaero.fr>
2022-12-14 20:20:17 +0100

    Implementing collectibles

--- src/Fruit.py
+++ src/Fruit.py
@@ -1,6 +1,12 @@
-import Collectible
+from Collectible import Collectible
 
 class Fruit(Collectible):
     
-    def __init__(self, x = 0, y = 0):
-        super.__init__(x, y)
+    fruits = [100, 300, 500, 700, 1000, 2000, 3000, 5000]
+    
+    def __init__(self, x = 0, y = 0, f = 0):  # f int describing the fruit
+        super.__init__(x, y, 4, 4 + f)
+        self.points = Fruit.fruits[f]
+        
+    def collect(self):
+        return (self.points, True, False) #Points / Should display points granted ? / Should make Pac-Man super ?
--- src/PacDot.py
+++ src/PacDot.py
@@ -1,6 +1,11 @@
-import Collectible
+from Collectible import Collectible
 
 class PacDot(Collectible):
     
+    points = 10
+    
     def __init__(self, x = 0, y = 0):
-        super.__init__(x, y)
+        super.__init__(x, y, 98, 98)
+    
+    def collect(self):
+            return (PacDot.points, False, False) #Points / Should display points granted ? / Should make Pac-Man super ?
