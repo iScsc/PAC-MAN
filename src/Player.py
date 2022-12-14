e843a06a545afbf91781ab455588e32dd01839c6
ZynoXelek <Enzo.BROSSIER-SECHER@student.isae-supaero.fr>
2022-12-14 20:23:13 +0100

    Creating Player class

--- src/Player.py
+++ src/Player.py
@@ -1,1 +1,20 @@
-#class Player:
+import pygame as pg
+
+class Player:
+    
+    keys = [pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN]
+    
+    points = 0
+    
+    @staticmethod
+    def getEvents():
+        
+        r = [0 for i in range(len(Player.keys))]
+        
+        for event in pg.event.get():
+            
+            if event.type in Player.keys:
+                r[Player.keys.index(event.type)] = 1
+            
+        return r
+    
